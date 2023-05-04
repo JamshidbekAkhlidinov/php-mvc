c331cfafb232132d4c6bee0ca970557c5e5a6ebe
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2023-05-04 18:48:21 +0500

    created main folder

--- public/index.php
+++ public/index.php
@@ -1,18 +1,24 @@
 <?php
+/*
+ *   Jamshidbek Akhlidinov
+ *   4 - 5 2023 12:54:40
+ *   https://github.com/JamshidbekAkhlidinov
+ */
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 use app\core\Application;
 
-require_once  "vendor/autoload.php";
+require_once  "../vendor/autoload.php";
 
-$app = new Application();
+$app = new Application(dirname(__DIR__));
 
-$app->router->get('/',function (){
-    return "Hello world";
-});
+$app->router->get('/','home');
 
-$app->router->get('/contact',function (){
-    return "Ths is page contact";
-});
+$app->router->get('/contact','contact');
 
 $app->run();
 

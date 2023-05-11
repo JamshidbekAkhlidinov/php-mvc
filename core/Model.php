a6673627d81f99c54a7bc3202b04e606060e167c
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2023-05-11 13:11:47 +0500

    crated form, filed

--- core/Model.php
+++ core/Model.php
@@ -9,7 +9,6 @@ namespace app\core;
 
 abstract class Model
 {
-
     public const RULE_REQUIRED = 'required';
     public const RULE_EMAIL = 'email';
     public const RULE_MIN = 'min';
@@ -42,9 +41,23 @@ abstract class Model
                     $this->addError($attribute, self::RULE_REQUIRED);
                 }
 
+                if ($ruleName === self::RULE_EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $this->addError($attribute, self::RULE_EMAIL);
+                }
+
+                if ($ruleName === self::RULE_MIN && strlen($value) < $rule['min']) {
+                    $this->addError($attribute, self::RULE_MIN, $rule);
+                }
+
+                if ($ruleName === self::RULE_MAX && strlen($value) > $rule['max']) {
+                    $this->addError($attribute, self::RULE_MAX, $rule);
+                }
+
+                if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
+                    $this->addError($attribute, self::RULE_MATCH, $rule);
+                }
             }
         }
-
         return empty($this->errors);
     }
 
@@ -61,11 +74,23 @@ abstract class Model
         ];
     }
 
-    public function addError($attribute, $rule)
+    public function addError($attribute, $rule, $params = [])
     {
         $message = $this->errorMessage()[$rule] ?? '';
+        foreach ($params as $key => $value) {
+            $message = str_replace("{{$key}}", $value, $message);
+        }
         $this->errors[$attribute][] = $message;
     }
 
+    public function hasError($attribute)
+    {
+        return $this->errors[$attribute] ?? false;
+    }
+
+    public function getFirstError($attibute)
+    {
+        return $this->errors[$attibute][0] ?? false;
+    }
 
 }
---
+++ core/form/Field.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ *   Jamshidbek Akhlidinov
+ *   11 - 5 2023 11:52:51
+ *   https://github.com/JamshidbekAkhlidinov
+ */
+
+namespace app\core\form;
+
+use app\core\Model;
+
+class Field
+{
+
+    public const TYPE_TEXT = 'text';
+    public const TYPE_PASSWORD = 'password';
+    public const TYPE_EMAIL = 'email';
+
+    public string $type;
+    public Model $model;
+    public string $attribute;
+
+    public function __construct(Model $model, $attribute)
+    {
+        $this->model = $model;
+        $this->type = self::TYPE_TEXT;
+        $this->attribute = $attribute;
+    }
+
+    public function __toString()
+    {
+        $model = $this->model;
+        $attribute = $this->attribute;
+        return sprintf('
+                <label class="form-label">%s</label>
+                <input type="%s" name="%s" value="%s" class="form-control %s" aria-describedby="dd">
+                <div class="invalid-feedback" id="dd">
+                    %s
+                </div>
+        ',
+            $attribute,
+            $this->type,
+            $attribute,
+            $model->{$attribute},
+            $model->hasError($attribute) ? 'is-invalid' : '',
+            $model->getFirstError($attribute),
+        );
+    }
+
+    public function passwordField()
+    {
+        $this->type = self::TYPE_PASSWORD;
+        return $this;
+    }
+}
